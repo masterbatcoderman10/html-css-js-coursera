794df2f19c2fa6572d06307667719fd33386e182
MAA089 <maa089@student.bham.ac.uk>
2022-04-06 20:43:01 +0400

    added quotes

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -100,8 +100,8 @@ $(function () {
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
         
-        var chosenCategoryShortName =
-          chooseRandomCategory(categories).short_name;
+        var chosenCategoryShortName = "'" +
+          chooseRandomCategory(categories).short_name + "'";
 
         // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
         // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -114,6 +114,7 @@ $(function () {
         // Hint: you need to surround the chosen category short name with something before inserting
         // it into the home html snippet.
         //
+
         console.log("Checking step 3");
         console.log(chosenCategoryShortName);
         var homeHtmlToInsertIntoMainPage = insertProperty(
